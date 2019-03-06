55c80f9bfcb4b1dd95d33c2a441b40406594a8bb
cosinus30 <krmtrk20@gmail.com>
2019-03-06 16:22:13 +0300

    TestUtil is done

--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.beans.Transient;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,4 +15,34 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void oneLengthArg(){
+    int [] arr = new int[1];
+    assertFalse(c.compute(arr));
+  }
+
+  @Test
+  public void evenLengthArg(){
+    int [] arr = new int[2];
+    assertFalse(c.compute(arr));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void exception(){
+    int [] arr = {0,1,2};
+    boolean value = c.compute(arr);
+  }
+
+  @Test
+  public void sumModiEqualsZero(){
+    int [] arr = {1,2,3};
+    assertTrue(c.compute(arr));
+  }
+
+  @Test
+  public void sumModiIsNotEqualsZero(){
+    int [] arr = {2,3,6};
+    assertFalse(c.compute(arr));
+  }
 }
